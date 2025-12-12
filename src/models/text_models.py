aa22cf02460bc774bc826c42323e687d7fe263d0
Aditi Gupta <147914930+AditiGupta2412@users.noreply.github.com>
2025-12-12 21:42:50 +0530

    Remove SimpleTextVectorizer class from text_models.py

--- src/models/text_models.py
+++ src/models/text_models.py
@@ -1,15 +1,1 @@
-# src/models/text_models.py
-from sklearn.feature_extraction.text import TfidfVectorizer
 
-class SimpleTextVectorizer:
-    def __init__(self, text_col='text', max_features=5000):
-        self.text_col = text_col
-        self.vectorizer = TfidfVectorizer(max_features=max_features)
-
-    def fit_transform(self, df):
-        docs = df[self.text_col].fillna('').astype(str).values
-        return self.vectorizer.fit_transform(docs)
-
-    def transform(self, df):
-        docs = df[self.text_col].fillna('').astype(str).values
-        return self.vectorizer.transform(docs)
