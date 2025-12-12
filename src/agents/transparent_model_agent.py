2f069044a148b039993fab96db05a61468ea1437
Aditi Gupta <147914930+AditiGupta2412@users.noreply.github.com>
2025-12-12 21:41:43 +0530

    Remove TransparentModelAgent class implementation

--- src/agents/transparent_model_agent.py
+++ src/agents/transparent_model_agent.py
@@ -1,16 +1,1 @@
-# src/agents/transparent_model_agent.py
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
-class TransparentModelAgent:
-    def __init__(self):
-        pass
-
-    def train(self, X_train, y_train, task='classification'):
-        if task in ('classification','text_classification'):
-            model = DecisionTreeClassifier(max_depth=4)
-            model.fit(X_train, y_train)
-            return model
-        else:
-            model = DecisionTreeRegressor(max_depth=4)
-            model.fit(X_train, y_train)
-            return model
